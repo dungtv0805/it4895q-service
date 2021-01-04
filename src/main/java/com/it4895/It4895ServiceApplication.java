2a350eae079da6e5257a430379342c1a579c4437
dungtv0805 <dung.tv0805@gmail.com>
2021-01-05 01:37:24 +0700

    hello world

---
+++ src/main/java/com/it4895/It4895ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.it4895;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It4895ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It4895ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/it4895/It4895ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it4895;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It4895ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
